c2ff73ec0c2606316a01e6b485ea865fa060099d
Deyky Pinheiro <pinheiromikael96@gmail.com>
2022-03-25 17:27:22 -0300

    orderby

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { AlertasService } from './../service/alertas.service';
+import { AuthService } from './../service/auth.service';
+import { Tema } from './../model/Tema';
+import { TemaService } from './../service/tema.service';
+import { PostagemService } from './../service/postagem.service';
+import { Postagem } from './../model/Postagem';
 import { Router } from '@angular/router';
-import { environment } from 'src/environments/environment.prod';
-import { Postagem } from '../model/Postagem';
-import { Tema } from '../model/Tema';
+import { environment } from './../../environments/environment.prod';
+import { Component, OnInit } from '@angular/core';
 import { Usuario } from '../model/Usuario';
-import { AlertasService } from '../service/alertas.service';
-import { AuthService } from '../service/auth.service';
-import { PostagemService } from '../service/postagem.service';
-import { TemaService } from '../service/tema.service';
 
 @Component({
 	selector: 'app-inicio',
@@ -18,32 +18,38 @@ export class InicioComponent implements OnInit {
 
 	postagem: Postagem = new Postagem()
 	listaPostagens: Postagem[]
+	tituloPost: string
 
 	tema: Tema = new Tema()
 	listaTemas: Tema[]
 	idTema: number
+	nomeTema: string
 
 	user: Usuario = new Usuario()
 	idUser = environment.id
+
+	key = 'data'
+	reverse = true
 
 	constructor(
 		private router: Router,
 		private postagemService: PostagemService,
 		private temaService: TemaService,
-		private authService: AuthService,
-		private alertas: AlertasService,
+		public authService: AuthService,
+		private alertas: AlertasService
 	) { }
 
 	ngOnInit() {
-
 		window.scroll(0, 0)
 
-		if (environment.token == "") {
-			this.router.navigate(["/entrar"]);
+		if (environment.token == '') {
+			this.router.navigate(['/entrar'])
 		}
 
-		this.getAllTemas();
-		this.getAllPostagem();
+		this.getAllTemas()
+		this.getAllPostagens()
+
+
 	}
 
 	getAllTemas() {
@@ -58,12 +64,17 @@ export class InicioComponent implements OnInit {
 		})
 	}
 
-	getAllPostagem(){
-		this.postagemService.getAllPostagem().subscribe((resp: Postagem[]) => {
-			this.listaPostagens = resp;
+	getAllPostagens() {
+		this.postagemService.getAllPostagens().subscribe((resp: Postagem[]) => {
+			this.listaPostagens = resp
 		})
 	}
 
+	findByIdUser() {
+		this.authService.getByIdUser(this.idUser).subscribe((resp: Usuario) => {
+			this.user = resp
+		})
+	}
 
 	publicar() {
 		this.tema.id = this.idTema
@@ -75,16 +86,29 @@ export class InicioComponent implements OnInit {
 		this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem) => {
 			this.postagem = resp
 			this.alertas.showAlertSuccess('Postagem realizada com sucesso!')
-			this.postagem = new Postagem();
-			this.getAllPostagem();
+			this.postagem = new Postagem()
+			this.getAllPostagens()
 		})
 	}
 
-	findByIdUser(){
-		this.authService.getByIdUser(this.idUser).subscribe((resp: Usuario) => {
-			this.user = resp;
+	findByTituloPostagem() {
+		if (this.tituloPost == '') {
+			this.getAllPostagens()
+		} else {
+			this.postagemService.getByTituloPostagem(this.tituloPost).subscribe((resp: Postagem[]) => {
+				this.listaPostagens = resp
+			})
+		}
+	}
 
-		})
+	findByNomeTema() {
+		if (this.nomeTema == '') {
+			this.getAllTemas()
+		} else {
+			this.temaService.getByNomeTema(this.nomeTema).subscribe((resp: Tema[]) => {
+				this.listaTemas = resp
+			})
+		}
 	}
 
 }
--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -1,7 +1,7 @@
+import { Observable } from 'rxjs';
+import { environment } from './../../environments/environment.prod';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { environment } from 'src/environments/environment.prod';
 import { Postagem } from '../model/Postagem';
 
 @Injectable({
@@ -9,32 +9,34 @@ import { Postagem } from '../model/Postagem';
 })
 export class PostagemService {
 
-	constructor(
-		private http: HttpClient
-	) { }
+	constructor(private http: HttpClient) { }
 
 	token = {
-		headers: new HttpHeaders().set("Authorization", environment.token)
+		headers: new HttpHeaders().set('Authorization', environment.token)
 	}
 
-
-	getAllPostagem(): Observable<Postagem[]>{
-		return this.http.get<Postagem[]>("http://localhost:8080/postagem", this.token);
+	getAllPostagens(): Observable<Postagem[]> {
+		return this.http.get<Postagem[]>('http://localhost:8080/postagens', this.token)
 	}
 
-	getByIdPostagem(id: number): Observable<Postagem>{
-		return this.http.get<Postagem>(`http://localhost:8080/postagem/${id}`, this.token);
+	getByIdPostagem(id: number): Observable<Postagem> {
+		return this.http.get<Postagem>(`http://localhost:8080/postagens/${id}`, this.token)
 	}
 
-	postPostagem(postagem: Postagem): Observable<Postagem>{
-		return this.http.post<Postagem>("http://localhost:8080/postagem", postagem, this.token);
+	getByTituloPostagem(titulo: string): Observable<Postagem[]> {
+		return this.http.get<Postagem[]>(`http://localhost:8080/postagens/titulo/${titulo}`, this.token)
 	}
 
-	putPostagem(postagem: Postagem): Observable<Postagem>{
-		return this.http.put<Postagem>("http://localhost:8080/postagem", postagem, this.token);
+	postPostagem(postagem: Postagem): Observable<Postagem> {
+		return this.http.post<Postagem>('http://localhost:8080/postagens', postagem, this.token)
 	}
 
-	deletePostagem(id: number){
-		return this.http.delete(`http://localhost:8080/postagem/${id}`, this.token);
+	putPostagem(postagem: Postagem): Observable<Postagem> {
+		return this.http.put<Postagem>('http://localhost:8080/postagens', postagem, this.token)
 	}
+
+	deletePostagem(id: number) {
+		return this.http.delete(`http://localhost:8080/postagens/${id}`, this.token)
+	}
+
 }
