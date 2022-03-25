2dcaa6ce09961e40c4488c0e1054da3bba2bfba2
Deyky Pinheiro <pinheiromikael96@gmail.com>
2022-03-25 17:10:30 -0300

    alertas reutilizaveis

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -4,10 +4,10 @@ import { environment } from 'src/environments/environment.prod';
 import { Postagem } from '../model/Postagem';
 import { Tema } from '../model/Tema';
 import { Usuario } from '../model/Usuario';
+import { AlertasService } from '../service/alertas.service';
 import { AuthService } from '../service/auth.service';
 import { PostagemService } from '../service/postagem.service';
 import { TemaService } from '../service/tema.service';
-import { AlertasService } from './../service/alertas.service';
 
 @Component({
 	selector: 'app-inicio',
@@ -32,7 +32,6 @@ export class InicioComponent implements OnInit {
 		private temaService: TemaService,
 		private authService: AuthService,
 		private alertas: AlertasService,
-
 	) { }
 
 	ngOnInit() {
@@ -75,7 +74,7 @@ export class InicioComponent implements OnInit {
 
 		this.postagemService.postPostagem(this.postagem).subscribe((resp: Postagem) => {
 			this.postagem = resp
-			this.alertas.showAlertSuccess('Postagem realizada com sucesso!');
+			this.alertas.showAlertSuccess('Postagem realizada com sucesso!')
 			this.postagem = new Postagem();
 			this.getAllPostagem();
 		})
